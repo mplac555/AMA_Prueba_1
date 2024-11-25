c0e1ead4fa35ec421046d3b56366b2ff110e044a
mplac555 <marcelo.placencia@epn.edu.ec>
2024-11-25 17:21:11 -0500

    Tarjeta

--- app/src/main/java/ec/edu/epn/prueba1/MainActivity.kt
+++ app/src/main/java/ec/edu/epn/prueba1/MainActivity.kt
@@ -1,47 +1,104 @@
 package ec.edu.epn.prueba1
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ec.edu.epn.prueba1.ui.theme.Prueba1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Prueba1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContent { MainContent() }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            Column {
+                Text("Eventos en \"San José\" por el feriado de noviembre")
+//                LazyVerticalGrid() {
+//
+//                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tarjeta(modifier: Modifier = Modifier, nombre: String = "", lugar: String = "", fecha: String = "", asistentes: Int = 0, icono: Painter = painterResource(R.drawable.ic_launcher_foreground)) {
+    Box(modifier.background(Color.LightGray, RoundedCornerShape(15.dp)).border(0.dp, Color.Transparent, RoundedCornerShape(15.dp)).padding(15.dp)) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Box(
+                    Modifier
+                        .height(75.dp)
+                        .aspectRatio(1f)
+                        .padding(10.dp)
+                        .background(Color.White, CircleShape)
+                        .border(1.dp, Color.Black, CircleShape)
+                ) {
+                    Icon(icono, "icono del evento",Modifier.align(Alignment.Center).fillMaxSize(.75f))
+                }
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = nombre, fontWeight = FontWeight.Black)
+                    Spacer(Modifier.height(15.dp))
+                    Text(lugar, fontWeight = FontWeight.Light, fontStyle = FontStyle.Italic)
+                    Text(fecha)
+                }
+            }
+            Spacer(Modifier.height(15.dp))
+            Text("$asistentes asistentes", Modifier.padding(start = 5.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Prueba1Theme {
-        Greeting("Android")
-    }
+fun MainPreview() {
+//    MainContent()
+    Tarjeta(
+        modifier = Modifier,
+        nombre = "Trail en el Mirador de la Perdiz",
+        lugar = "Mirador de la Perdiz",
+        fecha = "31/10/2024",
+        asistentes = 30,
+        icono = painterResource(R.drawable.ico_montana)
+    )
 }
