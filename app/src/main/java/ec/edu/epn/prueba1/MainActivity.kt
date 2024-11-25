e83406d73189bc851d97a37711707f7e797c185e
mplac555 <marcelo.placencia@epn.edu.ec>
2024-11-25 18:01:22 -0500

    Creación del gestor de la BD

--- app/src/main/java/ec/edu/epn/prueba1/MainActivity.kt
+++ app/src/main/java/ec/edu/epn/prueba1/MainActivity.kt
@@ -1,7 +1,13 @@
 package ec.edu.epn.prueba1
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -45,12 +51,42 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val eventos = mutableListOf<Evento>()
+        val cursor = DatabaseHelper(this).getAllPuntosInteres()
+        cursor?.use {
+            while (it.moveToNext()) {
+                val idIndex = it.getColumnIndex(DatabaseHelper.COLUMN_ID)
+                val nombreIndex = it.getColumnIndex(DatabaseHelper.COLUMN_NAME)
+                val lugarIndex = it.getColumnIndex(DatabaseHelper.COLUMN_PLACE)
+                val fechaIndex = it.getColumnIndex(DatabaseHelper.COLUMN_DATE)
+                val asistentesIndex = it.getColumnIndex(DatabaseHelper.COLUMN_ASSISTANTS)
+                if(
+                    idIndex != -1 &&
+                    nombreIndex != -1 &&
+                    lugarIndex != -1 &&
+                    fechaIndex != -1 &&
+                    asistentesIndex != -1
+                ) {
+                    val _id = it.getInt(idIndex)
+                    val nombre = it.getString(nombreIndex)
+                    val lugar = it.getString(lugarIndex)
+                    val fecha = it.getString(fechaIndex)
+                    val asistentes = it.getInt(asistentesIndex)
+                    eventos.add(Evento(_id, nombre, lugar, fecha, asistentes))
+                } else {
+                    Log.e("DatabaseError", "Una o más columnas no fueron encontradas en el cursor.")
+                }
+            }
+        }
+
         setContent { MainContent() }
     }
 }
 
 @Composable
 fun MainContent() {
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Box(Modifier.background(Color.Gray).padding(innerPadding)) {
             Column(
@@ -107,6 +143,75 @@ fun Tarjeta(modifier: Modifier = Modifier, nombre: String = "", lugar: String = 
             Spacer(Modifier.height(15.dp))
             Text("$asistentes asistentes", Modifier.padding(start = 5.dp))
         }
+    }
+}
+
+class Evento(
+    val _id : Int,
+    var nombre : String,
+    var lugar : String,
+    var fecha : String,
+    var asistentes : Int,
+    var imagen_id : Int = R.drawable.ic_launcher_foreground
+)
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        const val DATABASE_NAME = "EventosTuristicos.db"
+        const val DATABASE_VERSION = 1
+        const val TABLE_NAME = "eventos_noviembre"
+        const val COLUMN_ID = "_id"
+        const val COLUMN_NAME = "nombre"
+        const val COLUMN_PLACE = "lugar"
+        const val COLUMN_DATE = "fecha"
+        const val COLUMN_ASSISTANTS = "asistentes"
+//        const val COLUMN_IMAGE = "imagen"
+    }
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTable = """
+            CREATE TABLE $TABLE_NAME (
+            $COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            $COLUMN_NAME TEXT NOT NULL,
+            $COLUMN_PLACE TEXT NOT NULL,
+            $COLUMN_DATE TEXT NOT NULL,
+            $COLUMN_ASSISTANTS INTEGER NOT NULL
+            )
+        """
+        db.execSQL(createTable)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun insertPuntoInteres(
+        nombre: String,
+        lugar: String,
+        fecha: String,
+        asistentes: Int,
+//        imagen: Painter
+    ): Long {
+        val db = this.writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_NAME, nombre)
+            put(COLUMN_PLACE, lugar)
+            put(COLUMN_DATE, fecha)
+            put(COLUMN_ASSISTANTS, asistentes)
+//            put(COLUMN_IMAGE, imagen)
+        }
+        return db.insert(TABLE_NAME, null, values)
+    }
+    fun getAllPuntosInteres(): Cursor {
+        val db = this.readableDatabase
+        val projection = arrayOf(
+            COLUMN_NAME,
+            COLUMN_PLACE,
+            COLUMN_DATE,
+            COLUMN_ASSISTANTS
+//            COLUMN_IMAGE
+        )
+        return db.query(TABLE_NAME, null, null, null, null, null, null)
     }
 }
 
